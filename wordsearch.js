405a57772f31c34fd814515cbe55d02f7b109667
Liz Fosdick <lizfosdick@gmail.com>
2023-02-15 15:46:08 -0800

    submitting assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
 const wordSearch = (letters, word) => { 
+    
+  const transpose = function(letters) {
+    let newArray = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      newArray[i] = [];
+    }
+  
+    for (let row = 0; row < letters.length; row++) {
+      for (let co = 0; co < letters[row].length; co++) {
+        newArray[co][row] = letters[row][co];
+      }
+    }
+       const verticalJoin = newArray.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    };
+
+ 
+    
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
+    return false;
 }
+
+
+
 
 module.exports = wordSearch
